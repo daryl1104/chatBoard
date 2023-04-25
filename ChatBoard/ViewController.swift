5ecdc79ec6c66d9c9591168da7bfcc2385373d6e
易鑫 <yix29@chinaunicom.cn>
2023-04-25 11:58:08 +0800

    Initial Commit

---
+++ ChatBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatBoard
+//
+//  Created by daryl on 2023/4/25.
+//  Copyright © 2023 daryl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
